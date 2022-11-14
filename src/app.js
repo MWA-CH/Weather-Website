349579fffa1029ebb466d6f1e75fd37864607fe5
Muhammed Waqar Ahmed <m.waqar.ahmed@gmail.com>
2022-11-14 22:52:28 +0500

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define path for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -51,8 +52,8 @@ app.get('/weather', (req, res) => {
         forecast(latitude, longitude, (error, forecastData) => {
             if (error) return res.send({ error })
 
-            console.log(location)
-            console.log(forecastData)
+            //console.log(location)
+            //console.log(forecastData)
             res.send({
                 forecast: forecastData,
                 location,
@@ -89,6 +90,6 @@ app.get('*', (req, res) => {
         errorMessage: 'Page nor found.'
     })
 })
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
